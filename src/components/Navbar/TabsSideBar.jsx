6a6212bf6705e56c56a98f229f16da59673c6622
randa.ibrahim <randa.ibrahim@cartologic.com>
2023-11-15 01:09:09 +0200

    last update

--- src/components/Navbar/TabsSideBar.jsx
+++ src/components/Navbar/TabsSideBar.jsx
@@ -1,11 +1,49 @@
-import { Tab, Box } from "@mui/material";
-
+import { Tab, Box, TabContext, TabList, TabPanel } from "@mui/material";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 import LayersOutlinedIcon from "@mui/icons-material/LayersOutlined";
 import GpsFixedOutlinedIcon from "@mui/icons-material/GpsFixedOutlined";
 import GeoDataList from "../Areas/GeoDataList";
 import BaseMapsList from "../Areas/BaseMapsList";
 import MapServicesPanel from "../Areas/MapServicesPanel/MapServicesPanel";
+
+export default function LabTabs() {
+  const [value, setValue] = React.useState("0");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <TabContext value={value}>
+      <Box sx={headerTapsStyle}>
+        <TabList
+          onChange={handleChange}
+          centered
+          value={value}
+          aria-label="basic tabs example"
+          sx={{
+            "& *": { color: "white" },
+            "& .Mui-selected *": { color: "inherit  " },
+          }}
+        >
+          <Tab icon={<LayersOutlinedIcon />} value="0" />
+          <Tab icon={<GpsFixedOutlinedIcon />} value="1" />
+          <Tab icon={<MapOutlinedIcon />} value="2" />
+        </TabList>
+      </Box>
+
+      <TabPanel value="0">
+        <MapServicesPanel />
+      </TabPanel>
+      <TabPanel value="1">
+        <GeoDataList />
+      </TabPanel>
+      <TabPanel value="2">
+        <BaseMapsList />
+      </TabPanel>
+    </TabContext>
+  );
+}
 
 const headerTapsStyle = {
   boxShadow: 1,
@@ -15,49 +53,3 @@ const headerTapsStyle = {
   bgcolor: "secondary.main",
   zIndex: 10,
 };
-
-import * as React from "react";
-
-import TabContext from "@mui/lab/TabContext";
-import TabList from "@mui/lab/TabList";
-import TabPanel from "@mui/lab/TabPanel";
-
-export default function LabTabs() {
-  const [value, setValue] = React.useState("1");
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-      <TabContext value={value}>
-        <Box sx={headerTapsStyle}>
-          <TabList
-            onChange={handleChange}
-            centered
-            value={value}
-            aria-label="basic tabs example"
-            sx={{
-              "& *": { color: "white" },
-              "& .Mui-selected *": { color: "inherit  " },
-            }}
-          >
-            <Tab icon={<LayersOutlinedIcon />} value="0" />
-            <Tab icon={<GpsFixedOutlinedIcon />} value="1" />
-            <Tab icon={<MapOutlinedIcon />} value="2" />
-          </TabList>
-        </Box>
-
-        <TabPanel value="0">
-          <MapServicesPanel />
-        </TabPanel>
-        <TabPanel value="1">
-          <GeoDataList />
-        </TabPanel>
-        <TabPanel value="2">
-          <BaseMapsList />
-        </TabPanel>
-      </TabContext>
-  
-  );
-}
