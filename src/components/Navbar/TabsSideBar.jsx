b01b1a855535b39ca88de778e426294fac162580
randa.ibrahim <randa.ibrahim@cartologic.com>
2023-11-15 01:16:45 +0200

    clean tabs

--- src/components/Navbar/TabsSideBar.jsx
+++ src/components/Navbar/TabsSideBar.jsx
@@ -1,49 +1,12 @@
-import { Tab, Box, TabContext, TabList, TabPanel } from "@mui/material";
+import { useState } from "react";
+import { Tab, Box } from "@mui/material";
+import {TabContext , TabList ,TabPanel } from "@mui/lab";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 import LayersOutlinedIcon from "@mui/icons-material/LayersOutlined";
 import GpsFixedOutlinedIcon from "@mui/icons-material/GpsFixedOutlined";
 import GeoDataList from "../Areas/GeoDataList";
 import BaseMapsList from "../Areas/BaseMapsList";
 import MapServicesPanel from "../Areas/MapServicesPanel/MapServicesPanel";
-
-export default function LabTabs() {
-  const [value, setValue] = React.useState("0");
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <TabContext value={value}>
-      <Box sx={headerTapsStyle}>
-        <TabList
-          onChange={handleChange}
-          centered
-          value={value}
-          aria-label="basic tabs example"
-          sx={{
-            "& *": { color: "white" },
-            "& .Mui-selected *": { color: "inherit  " },
-          }}
-        >
-          <Tab icon={<LayersOutlinedIcon />} value="0" />
-          <Tab icon={<GpsFixedOutlinedIcon />} value="1" />
-          <Tab icon={<MapOutlinedIcon />} value="2" />
-        </TabList>
-      </Box>
-
-      <TabPanel value="0">
-        <MapServicesPanel />
-      </TabPanel>
-      <TabPanel value="1">
-        <GeoDataList />
-      </TabPanel>
-      <TabPanel value="2">
-        <BaseMapsList />
-      </TabPanel>
-    </TabContext>
-  );
-}
 
 const headerTapsStyle = {
   boxShadow: 1,
@@ -53,3 +16,43 @@ const headerTapsStyle = {
   bgcolor: "secondary.main",
   zIndex: 10,
 };
+
+export default function LabTabs() {
+  const [value, setValue] = useState("0");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+      <TabContext value={value}>
+        <Box sx={headerTapsStyle}>
+          <TabList
+            onChange={handleChange}
+            centered
+            value={value}
+            aria-label="basic tabs example"
+            sx={{
+              "& *": { color: "white" },
+              "& .Mui-selected *": { color: "inherit  " },
+            }}
+          >
+            <Tab icon={<LayersOutlinedIcon />} value="0" />
+            <Tab icon={<GpsFixedOutlinedIcon />} value="1" />
+            <Tab icon={<MapOutlinedIcon />} value="2" />
+          </TabList>
+        </Box>
+
+        <TabPanel value="0">
+          <MapServicesPanel />
+        </TabPanel>
+        <TabPanel value="1">
+          <GeoDataList />
+        </TabPanel>
+        <TabPanel value="2">
+          <BaseMapsList />
+        </TabPanel>
+      </TabContext>
+  
+  );
+}
