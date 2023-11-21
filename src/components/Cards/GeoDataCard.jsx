70dee6dd2b372249f54bf8339f3b7df5150ce35a
randa.ibrahim <randa.ibrahim@cartologic.com>
2023-11-21 17:23:54 +0200

    update card style

--- src/components/Cards/GeoDataCard.jsx
+++ src/components/Cards/GeoDataCard.jsx
@@ -3,10 +3,15 @@ import { Card, Typography } from "@mui/material";
 export default function GeoDataCard(props) {
   return (
     <Card sx={cardStyle}>
-      <Typography sx={{ fontSize: 13 }} color="primary.main" gutterBottom>
-        {props.title}
+      <Typography sx={titleStyle} gutterBottom>
+        {props.title?.replaceAll("_", " ")}
       </Typography>
-      <Typography variant="h4" sx={{ fontSize: "1.7rem" }} color="info.main">
+      <Typography
+        noWrap
+        variant="h4"
+        sx={{ fontSize: "1.35rem" }}
+        color="info.main"
+      >
         {props.data || 0}
       </Typography>
     </Card>
@@ -16,7 +21,14 @@ export default function GeoDataCard(props) {
 const cardStyle = {
   color: "secondary.contrastText",
   px: 1.5,
-  py : .75,
+  py: 0.75,
   mb: 2,
   "& .MuiCardHeader-action": { m: 0 },
+};
+
+const titleStyle = {
+  fontSize: 11,
+  fontWeight: 800,
+  textTransform: "capitalize",
+  color: "primary.main",
 };
