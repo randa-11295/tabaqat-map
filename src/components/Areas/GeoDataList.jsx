7d87b365f75b2f954e8f9469423f4b740ef7bbf7
randa.ibrahim <randa.ibrahim@cartologic.com>
2023-11-25 16:55:57 +0200

    add no result area to geo location area

--- src/components/Areas/GeoDataList.jsx
+++ src/components/Areas/GeoDataList.jsx
@@ -2,7 +2,7 @@ import GeoDataCard from "../Cards/GeoDataCard";
 import { Grid, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import useConfig from "../../utils/config";
-
+import NoResult from "./NoResult";
 
 const GeoDataList = (props) => {
   const { t } = useTranslation();
@@ -10,40 +10,40 @@ const GeoDataList = (props) => {
 
   return (
     <Grid container columnSpacing={1.5}>
-     
-      <Grid item xs={12}>
-        <Typography component="h6" mb={1} variant="h6" sx={{ mb: 2 }}>
-          {t(`geoData.coordinates`)}
-        </Typography>
-      </Grid>
-      {props.data?.geometry?.coordinates?.map((valEl, indx) => (
-        <Grid item xs={6} key={valEl}>
-          <GeoDataCard
-            title={t(`geoData.${geoDataTitles.coordinates[indx]}`)}
-            data={valEl.toFixed(2)}
-          />
+     { props.data ?
+      <>
+        <Grid item xs={12}>
+          <Typography component="h6" mb={1} variant="h6" sx={{ mb: 2 }}>
+            {t(`geoData.coordinates`)}
+          </Typography>
         </Grid>
-      ))}
-      <Grid item xs={12}>
-        <Typography component="h6" mb={1} variant="h6" sx={{ mb: 2 }}>
-          {t(`geoData.bBox`)}
-        </Typography>
-      </Grid>
-      {props.data?.bbox?.map((valEl, indx) => (
-        <Grid item xs={6} key={valEl}>
-          <GeoDataCard
-            title={t(`geoData.${geoDataTitles.bBox[indx]}`)}
-            data={valEl.toFixed(2)}
-          />
+        {props.data?.geometry?.coordinates?.map((valEl, indx) => (
+          <Grid item xs={6} key={valEl}>
+            <GeoDataCard
+              title={t(`geoData.${geoDataTitles.coordinates[indx]}`)}
+              data={valEl.toFixed(2)}
+            />
+          </Grid>
+        ))}
+        <Grid item xs={12}>
+          <Typography component="h6" mb={1} variant="h6" sx={{ mb: 2 }}>
+            {t(`geoData.bBox`)}
+          </Typography>
         </Grid>
-      ))}
-      <Grid item xs={12}>
-        <Typography component="h6" mb={1} variant="h6" sx={{ mb: 2 }}>
-          {t(`geoData.features`)}
-        </Typography>
-      </Grid>
-      {props.data?.properties &&
-        Object.entries(props.data.properties).map(
+        {props.data?.bbox?.map((valEl, indx) => (
+          <Grid item xs={6} key={valEl}>
+            <GeoDataCard
+              title={t(`geoData.${geoDataTitles.bBox[indx]}`)}
+              data={valEl.toFixed(2)}
+            />
+          </Grid>
+        ))}
+        <Grid item xs={12}>
+          <Typography component="h6" mb={1} variant="h6" sx={{ mb: 2 }}>
+            {t(`geoData.features`)}
+          </Typography>
+        </Grid>
+        {Object.entries(props.data.properties).map(
           ([opjKey, value]) =>
             isString(value) && (
               <Grid item xs={6} key={opjKey + value}>
@@ -51,6 +51,12 @@ const GeoDataList = (props) => {
               </Grid>
             )
         )}
+      </>
+      :
+      <Grid item xs={12}>
+        <NoResult />
+      </Grid>
+}
     </Grid>
   );
 };
